a3babb22ae3217b4752d919b2a81deb9191b1c19
thegreedyturtle <thegreedyturtle@gmail.com>
2021-10-26 18:37:58 -0700

    tempConversion excersize completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(fIn) {
+  if (typeof(fIn)=='number') {
+    return (Math.round((fIn-32)*50/9)/10);
+    } else {
+      return "ERROR";
+    };
 };
 
-const ctof = function() {
-
+const ctof = function(cIn) {
+  if (typeof(cIn)=='number') {
+    return (Math.round(10*((cIn*9/5)+32))/10);
+  } else {
+    return "ERROR";
+  };
 };
-
+console.log(ftoc(32));
 // Do not edit below this line
 module.exports = {
   ftoc,
