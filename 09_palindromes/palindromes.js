4e076c748d5882b0c1155f8251139660602408f7
thegreedyturtle <thegreedyturtle@gmail.com>
2021-11-08 18:30:14 -0800

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    let stripped = palindrome.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+    if (stripped == stripped.split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
