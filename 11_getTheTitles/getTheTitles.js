5b62d48bdea056dbcc6424a595850ad60654cb9e
thegreedyturtle <thegreedyturtle@gmail.com>
2021-11-08 21:56:53 -0800

    Done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookIn) {
+    let output = [];
+    for (let i = 0; i<bookIn.length; i++) {
+        output.push(`${bookIn[i]['title']}`);
+    }
+    return output;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
 
 // Do not edit below this line
 module.exports = getTheTitles;
