5489abcf006a42ccbbd03bb2dba1555d40eb6d5c
thegreedyturtle <thegreedyturtle@gmail.com>
2021-11-08 17:04:06 -0800

    Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+
+
+const add = function(x,y) {
+  //Add x and y
+  let sum = x + y;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+  let sum = x - y;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+  let sum = 0;
+  for (i=0;i<inputArray.length; i++) {
+    sum += inputArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(theArguments) {
+  let times = 1;
+  for (let i=0; i<theArguments.length; i++) {
+    times *= theArguments[i];
+  }
+  return times;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let factorial = 1;
+  for (i=x;i>0;i--) {
+    factorial = i * factorial;
+  };
+  return factorial;
 };
 
 // Do not edit below this line
